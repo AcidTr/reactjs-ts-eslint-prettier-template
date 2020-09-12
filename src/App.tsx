d3eecba947bf64f81466da3a5be09ade0b656eba
Victor Mourão <pvictormourao@gmail.com>
2020-09-12 15:12:24 -0300

    Cleaning the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <h1>ReactJS Typescript Template</h1>
+    <p>Editorconfig, ESlint and Prettier Already configured!</p>
+  </>
+);
 
 export default App;
